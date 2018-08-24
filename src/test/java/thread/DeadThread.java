f8cc5bb526496db369a8591ed6122343ca347c64
lifan <lifan@bmkp.cn>
2018-08-24 15:28:26 +0800

    update

--- src/test/java/thread/DeadThread.java
+++ src/test/java/thread/DeadThread.java
@@ -1,4 +1,47 @@
 package thread;
 
-public class DeadThread {
+public class DeadThread implements Runnable{
+
+    int flag=1;
+    private static Object a = new Object();
+    private static Object b = new Object();
+
+    public void run(){
+        System.out.println("开始执行"+this.flag);
+        if(flag==1){
+            synchronized(a){
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                synchronized(b){
+                    System.out.println("flag1成功");
+                }
+            }
+        }
+        if(flag==0){
+            synchronized(b){
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                synchronized(a){
+                    System.out.println("flag2成功");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        DeadThread dt1 = new DeadThread();
+        DeadThread dt2 = new DeadThread();
+        Thread t1 = new Thread(dt1);
+        Thread t2 = new Thread(dt2);
+        dt1.flag=0;
+        dt2.flag=1;
+        t1.start();
+        t2.start();
+    }
 }
--- src/test/java/thread/ProducerConsumer.java
+++ src/test/java/thread/ProducerConsumer.java
@@ -1,4 +1,104 @@
 package thread;
 
 public class ProducerConsumer {
+    public static void main(String[] args){
+        Basket bk = new Basket();
+        Producer p = new Producer(bk);
+        Consumer c = new Consumer(bk);
+
+        new Thread(p).start();
+        new Thread(p).start();
+        new Thread(p).start();
+        new Thread(c).start();
+    }
+
+}
+
+
+class Wotou{
+    int id;
+
+    Wotou(int id){
+        this.id= id;
+    }
+
+    public String  toString(){
+        return "Wotou:"+id;
+    }
+}
+
+class Basket{
+
+    int index=0;
+
+    Wotou[] arrWT = new Wotou[6];
+
+    public synchronized void push(Wotou wt){
+        if(index == arrWT.length){
+            try{
+                this.wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        this.notify();
+        arrWT[index]=wt;
+        index++;
+    }
+
+    public synchronized Wotou pull(){
+        if(index == 0){
+            try{
+                this.wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        this.notify();
+        index--;
+        return arrWT[index];
+    }
+}
+
+class Producer implements Runnable{
+
+    Basket bk = null;
+
+    Producer(Basket bk){
+        this.bk = bk;
+    }
+
+    public void run(){
+        for(int i=0; i<20; i++){
+            Wotou wt = new Wotou(i);
+            bk.push(wt);
+            System.out.println("生产了：" + wt);
+            try{
+                Thread.sleep((int)(Math.random()*1000));
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Consumer implements Runnable{
+
+    Basket bk = null;
+
+    Consumer(Basket bk){
+        this.bk = bk;
+    }
+
+    public void run(){
+        for(int i=0; i<20; i++){
+            Wotou wt = bk.pull();
+            System.out.println("消费了：" + wt);
+            try{
+                Thread.sleep((int)(Math.random()*1000));
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
